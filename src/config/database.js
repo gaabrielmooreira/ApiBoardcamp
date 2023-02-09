678e59ccfaae6cee0ee3de9ba941063e43780114
gaabrielmooreira <gaabrielmooreira@gmail.com>
2023-02-09 14:13:27 -0300

    fix: database connection

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,16 @@
-const connection = new Pool({
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { Pool } = pg;
+
+const configDb = {
     connectionString: process.env.DATABASE_URL
-});
+};
+
+if (process.env.MODE === "prod") configDb.ssl = true;
+
+const db = new Pool(configDb);
+
+export default db;
