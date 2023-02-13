caca07aba5e79f56412889dc8008b70e5d8d6609
gaabrielmooreira <gaabrielmooreira@gmail.com>
2023-02-13 12:30:49 -0300

    fix: stockTotal min(1)

--- src/schemas/gameSchema.js
+++ src/schemas/gameSchema.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 const gameSchema = Joi.object({
     name: Joi.string().required(),
     image: Joi.string().required(),
-    stockTotal: Joi.number().required(),
+    stockTotal: Joi.number().min(1).required(),
     pricePerDay: Joi.number().required()
 });
 
